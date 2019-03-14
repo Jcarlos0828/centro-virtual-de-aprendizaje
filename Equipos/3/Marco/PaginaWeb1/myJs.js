84f9472c376c190401cef62c692465489513ac86
MarcoFarias <marcofariasmx@gmail.com>
2019-03-13 18:32:17 -0600

    actualizaciON

---
+++ Equipos/3/Marco/PaginaWeb1/myJs.js
@@ -0,0 +1,13 @@
+var totalSteps = 4;
+
+$('#myCarousel').carousel();
+$('#myCarousel').on('slide.bs.carousel', function (e) {
+  
+  //update progress
+  var step = $(e.relatedTarget).data('step');
+  var percent = (parseInt(step) / totalSteps) * 100;
+  
+  $('.progress-bar').css({width: percent + '%'});
+  $('.progress-bar').text("Step " + step);
+  
+})
