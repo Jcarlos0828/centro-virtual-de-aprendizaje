34014b45a69d57d0d4ab08d439ffcd1248076f97
Mariano Alipi <marianoalipi@gmail.com>
2019-03-30 12:25:48 -0600

    Added quiz to unidad1.html.

---
+++ Equipos/1/Mariano/intro_compu_kids/quiz.js
@@ -0,0 +1,34 @@
+$('input[name="pregunta_1"][type="button"]').on('click', function(event){
+    let respuesta = $('input[name="respuesta"][type="radio"]:checked').val();
+    if (respuesta === "incorrect"){
+        alert('Respuesta incorrecta.');
+    } else {
+      alert('¡Respuesta correcta!');
+    }
+    event.preventDefault();
+});
+
+$('input[name="pregunta_2"][type="button"]').on('click', function(event){
+    let correctas = $('input[name="respuesta"][type="checkbox"][value="correct"]').toArray().length;
+    let res_cor = $('input[name="respuesta"][type="checkbox"][value="correct"]:checked').toArray().length;
+    let res_incor = $('input[name="respuesta"][type="checkbox"][value="incorrect"]:checked').toArray().length;
+    if (res_incor > 0) {
+        alert('Al menos una de las opciones es incorrecta.');
+    } else if(res_cor !== correctas && res_cor > 0){
+        alert('Faltan opciones correctas.');
+    } else {
+        alert('¡Respuesta correcta!');
+    }
+    event.preventDefault();
+});
+
+$('input[name="pregunta_3"][type="button"]').on('click', function(event){
+    let respuesta = parseFloat($('input[name="respuesta"][type="number"]').val());
+    // respuestaCorrecta es la respuesta
+    let respuestaCorrecta = 100.21;
+    let precision = 2;
+    if (respuesta.toFixed(precision) !== respuestaCorrecta.toFixed(precision) && !isNaN(respuesta)){
+        alert('Respuesta incorrecta');
+    }
+    event.preventDefault();
+});
