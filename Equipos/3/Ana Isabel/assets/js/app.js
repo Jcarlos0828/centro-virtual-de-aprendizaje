60a85e409fc7dc06ace000681300f0722973d836
Isabel Cruz <isabel.cruz81699@gmail.com>
2019-04-13 17:45:44 -0500

    version nueva

--- Equipos/3/Ana Isabel/assets/js/app.js
+++ Equipos/3/Ana Isabel/assets/js/app.js
@@ -7,6 +7,7 @@
 	function sideNav() {
 		$('.side-nav .side-nav-menu li a').on('click', function(e) {
 			if ($(this).parent().hasClass("open")) {
+
 				$(this).parent().children('.dropdown-menu').slideUp(200, function() {
 					$(this).parent().removeClass("open");
 				});
@@ -14,10 +15,8 @@
 			} else {
 				$(this).parent().parent().children('li.open').children('.dropdown-menu').slideUp(200);
 				$(this).parent().parent().children('li.open').children('a').removeClass('open');
-
 				$(this).parent().parent().children('li.open').removeClass("open");
 				$(this).parent().children('.dropdown-menu').slideDown(200, function() {
-
 					$(this).parent().addClass("open");
 				});
 			}
@@ -28,16 +27,14 @@
 	function sideNavToggle() {
 		$('.side-nav-toggle').on('click', function(e) {
 	        $('.app').toggleClass("is-collapsed");
-
-			e.preventDefault();
+	        e.preventDefault();
 	    });
 	}	
 
 	// Side Panel Toogle
 	function sidePanelToggle() {
 		$('.side-panel-toggle').on('click', function(e) {
-			$('.side-panel').toggleClass("side-panel-open");
-
+	        $('.side-panel').toggleClass("side-panel-open");
 	        e.preventDefault();
 	    });
 	}	
