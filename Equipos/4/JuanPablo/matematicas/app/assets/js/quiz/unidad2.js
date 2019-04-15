9bd19c0a524455febb5627d97a67cd587dbb4991
Juan Pablo Galaz Chávez <juanpablogalazchavez@Juans-MacBook-Pro-4.local>
2019-04-14 20:31:46 -0500

    Update

---
+++ Equipos/4/JuanPablo/matematicas/app/assets/js/quiz/unidad2.js
@@ -0,0 +1,240 @@
+$('input[name="u2p1"][type="button"]').on('click', function(event){
+    let i1 = $('input[name="u2p1i1"]:checked').val();
+    let i2 = $('input[name="u2p1i2"]:checked').val();
+    if (i1 === "c" && i2 === "c"){
+        alert('Felicidades tienes todos los incisos correctos.');
+    }
+    else{
+        if (i1 === "i" && i2 === "i"){
+            alert('Incisos incorrectos');
+        }
+        else{
+            if (i1 === "i"){
+                alert('Inciso 1 incorrecto');
+            }
+            if (i2 === "i"){
+                alert('Inciso 2 incorrecto');
+            }
+        }
+    }
+    event.preventDefault();
+});
+
+$('input[name="u2p2"][type="button"]').on('click', function(event){
+    let i1 = $('input[name="u2p2i1"]:checked').val();
+    let i2 = $('input[name="u2p2i2"]:checked').val();
+    let i3 = $('input[name="u2p2i3"]:checked').val();
+    if (i1 === "c" && i2 === "c" && i3 === "c" ){
+        alert('Felicidades tienes todos los incisos correctos.');
+    }
+    else{
+        if (i1 === "i" && i2 === "i" && i3 === "i3" ||
+            i1 === "i" && i2 === "i" ||
+            i1 === "i" && i3 === "i" ||
+            i2 === "i" && i3 === "i" ){
+            alert('Incisos incorrectos');
+        }
+        else{
+            if (i1 === "i"){
+                alert('Inciso 1 incorrecto');
+            }
+            if (i2 === "i"){
+                alert('Inciso 2 incorrecto');
+            }
+            if (i3 === "i"){
+                alert('Inciso 3 incorrecto');
+            }
+        }
+    }
+    event.preventDefault();
+});
+
+$('input[name="u2p3"][type="button"]').on('click', function(event){
+    let i1 = $('input[name="u2p3i1"]:checked').val();
+    let i2 = $('input[name="u2p3i2"]:checked').val();
+    let i3 = $('input[name="u2p3i3"]:checked').val();
+    if (i1 === "c" && i2 === "c" && i3 === "c" ){
+        alert('Felicidades tienes todos los incisos correctos.');
+    }
+    else{
+        if (i1 === "i" && i2 === "i" && i3 === "i3" ||
+            i1 === "i" && i2 === "i" ||
+            i1 === "i" && i3 === "i" ||
+            i2 === "i" && i3 === "i" ){
+            alert('Incisos incorrectos');
+        }
+        else{
+            if (i1 === "i"){
+                alert('Inciso 1 incorrecto');
+            }
+            if (i2 === "i"){
+                alert('Inciso 2 incorrecto');
+            }
+            if (i3 === "i"){
+                alert('Inciso 3 incorrecto');
+            }
+        }
+    }
+    event.preventDefault();
+});
+
+$('input[name="u2p4"][type="button"]').on('click', function(event){
+    let i1 = $('input[name="u2p4i1"]:checked').val();
+    let i2 = $('input[name="u2p4i2"]:checked').val();
+    let i3 = $('input[name="u2p4i3"]:checked').val();
+    if (i1 === "c" && i2 === "c" && i3 === "c" ){
+        alert('Felicidades tienes todos los incisos correctos.');
+    }
+    else{
+        if (i1 === "i" && i2 === "i" && i3 === "i3" ||
+            i1 === "i" && i2 === "i" ||
+            i1 === "i" && i3 === "i" ||
+            i2 === "i" && i3 === "i" ){
+            alert('Incisos incorrectos');
+        }
+        else{
+            if (i1 === "i"){
+                alert('Inciso 1 incorrecto');
+            }
+            if (i2 === "i"){
+                alert('Inciso 2 incorrecto');
+            }
+            if (i3 === "i"){
+                alert('Inciso 3 incorrecto');
+            }
+        }
+    }
+    event.preventDefault();
+});
+
+$('input[name="u2p5"][type="button"]').on('click', function(event){
+    let i1 = $('input[name="u2p5i1"]:checked').val();
+    let i2 = $('input[name="u2p5i2"]:checked').val();
+    let i3 = $('input[name="u2p5i3"]:checked').val();
+    if (i1 === "c" && i2 === "c" && i3 === "c" ){
+        alert('Felicidades tienes todos los incisos correctos.');
+    }
+    else{
+        if (i1 === "i" && i2 === "i" && i3 === "i3" ||
+            i1 === "i" && i2 === "i" ||
+            i1 === "i" && i3 === "i" ||
+            i2 === "i" && i3 === "i" ){
+            alert('Incisos incorrectos');
+        }
+        else{
+            if (i1 === "i"){
+                alert('Inciso 1 incorrecto');
+            }
+            if (i2 === "i"){
+                alert('Inciso 2 incorrecto');
+            }
+            if (i3 === "i"){
+                alert('Inciso 3 incorrecto');
+            }
+        }
+    }
+    event.preventDefault();
+});
+
+$('input[name="u2p6"][type="button"]').on('click', function(event){
+    let i1 = $('input[name="u2p6i1"]:checked').val();
+    if (i1 === "c"){
+        alert('Felicidades tienes el inciso correcto.');
+    }
+    else{
+        alert('Inciso incorrecto');
+    }
+    event.preventDefault();
+});
+
+$('input[name="u2p7"][type="button"]').on('click', function(event){
+    let i1 = $('input[name="u2p7i1"]:checked').val();
+    if (i1 === "c"){
+        alert('Felicidades tienes el inciso correcto.');
+    }
+    else{
+        alert('Inciso incorrecto');
+    }
+    event.preventDefault();
+});
+
+$('input[name="u2p8"][type="button"]').on('click', function(event){
+    let i1 = $('input[name="u2p8i1"]:checked').val();
+    if (i1 === "c"){
+        alert('Felicidades tienes el inciso correcto.');
+    }
+    else{
+        alert('Inciso incorrecto');
+    }
+    event.preventDefault();
+});
+
+$('input[name="u2p9"][type="button"]').on('click', function(event){
+    let i1 = $('input[name="u2p9i1"]:checked').val();
+    let i2 = $('input[name="u2p9i2"]:checked').val();
+    if (i1 === "c" && i2 === "c"){
+        alert('Felicidades tienes todos los incisos correctos.');
+    }
+    else{
+        if (i1 === "i" && i2 === "i"){
+            alert('Incisos incorrectos');
+        }
+        else{
+            if (i1 === "i"){
+                alert('Inciso 1 incorrecto');
+            }
+            if (i2 === "i"){
+                alert('Inciso 2 incorrecto');
+            }
+        }
+    }
+    event.preventDefault();
+});
+
+$('input[name="u2p10"][type="button"]').on('click', function(event){
+    let i1 = $('input[name="u2p10i1"]:checked').val();
+    let i2 = $('input[name="u2p10i2"]:checked').val();
+    if (i1 === "c" && i2 === "c"){
+        alert('Felicidades tienes todos los incisos correctos.');
+    }
+    else{
+        if (i1 === "i" && i2 === "i"){
+            alert('Incisos incorrectos');
+        }
+        else{
+            if (i1 === "i"){
+                alert('Inciso 1 incorrecto');
+            }
+            if (i2 === "i"){
+                alert('Inciso 2 incorrecto');
+            }
+        }
+    }
+    event.preventDefault();
+});
+
+
+$('input[name="u2a1"][type="button"]').on('click', function(event){
+    let i1 = $('input[name="u2a1i1"]:checked').val();
+    event.preventDefault();
+});
+
+$('input[name="u2a2"][type="button"]').on('click', function(event){
+    let i1 = $('input[name="u2a2i1"]:checked').val();
+    event.preventDefault();
+});
+
+$('input[name="u2a3"][type="button"]').on('click', function(event){
+    let i1 = $('input[name="u2a3i1"]:checked').val();
+    event.preventDefault();
+});
+
+$('input[name="u2a4"][type="button"]').on('click', function(event){
+    let i1 = $('input[name="u2a4i1"]:checked').val();
+    event.preventDefault();
+});
+
+$('input[name="u2a5"][type="button"]').on('click', function(event){
+    let i1 = $('input[name="u2a5i1"]:checked').val();
+    event.preventDefault();
+});
